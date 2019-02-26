a5c11b01685db991a5fdbd53c5e330e5b9c4ddb1
Fernando-belitama <mono.dj.fernando@hotmail.com>
2019-02-26 00:03:46 -0500

    Proyecto(5to Sistemas) Autores: Fernando Belitama y Rai Panchi

---
+++ src/com/AlejandroTX/vista/Aplicacion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.AlejandroTX.vista;
+
+/**
+ *
+ * @author RAI
+ */
+public class Aplicacion {
+//    public static void main(String[] args) {
+//        Conexion c = new Conexion();
+//        c.conex();
+//    }
+    
+}
